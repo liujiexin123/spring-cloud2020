ddb1f8b2736b810377de2c6320be19b4529408e5
liujiexin123 <1506095514@qq.com>
2020-03-25 01:26:30 +0800

    修改支付模块

---
+++ spring-cloud2020/cloud-provider-payment8001/src/main/java/com/liujiexin/springcloud/PaymentMain8001.java
@@ -0,0 +1,26 @@
+package com.liujiexin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+
+/**
+ * ClassName PaymentMain8001
+ *
+ * @Description 支付模块启动类
+ * @Author liu jiexin
+ * Date 2020/3/17 0017 0:11
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class PaymentMain8001
+{
+    public static void main(String[] args) {
+
+        SpringApplication.run(PaymentMain8001.class,args);
+
+    }
+
+}
