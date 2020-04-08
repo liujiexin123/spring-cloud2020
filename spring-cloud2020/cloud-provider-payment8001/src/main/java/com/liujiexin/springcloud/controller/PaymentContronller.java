c95a2a5fbe13cf5a902a99eec8fdde8a3a996149
liujiexin123 <1506095514@qq.com>
2020-04-08 23:42:37 +0800

    增加 端口获取

--- spring-cloud2020/cloud-provider-payment8001/src/main/java/com/liujiexin/springcloud/controller/PaymentContronller.java
+++ spring-cloud2020/cloud-provider-payment8001/src/main/java/com/liujiexin/springcloud/controller/PaymentContronller.java
@@ -5,6 +5,7 @@ import com.liujiexin.springcloud.entities.Payment;
 import com.liujiexin.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -22,13 +23,16 @@ public class PaymentContronller
     @Autowired
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @PostMapping("/payment/create")
     public CommonResult create(@RequestBody Payment payment)
     {
         int result = paymentService.create(payment);
         log.info("********插入结果*********："+ result);
         if(result > 0){
-            return new CommonResult(200, "插入成功", result);
+            return new CommonResult(200, "插入成功,serverPort:"+serverPort, result);
         }else {
             return new CommonResult(444, "插入失败", null);
         }
@@ -41,7 +45,7 @@ public class PaymentContronller
         Payment payment = paymentService.getPaymentById(id);
         log.info("********查询结果*********："+ payment);
         if(payment != null){
-            return new CommonResult(200, "查询成功22", payment);
+            return new CommonResult(200, "查询成功,serverPort:"+serverPort, payment);
         }else {
             return new CommonResult(444, "没有对应记录，查询id", id);
         }
